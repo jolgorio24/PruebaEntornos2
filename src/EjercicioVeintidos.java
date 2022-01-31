4f328cfcd769550d256303780a89964516b82556
jolgorio24 <rubenabadsanchez94@gmail.com>
2022-01-31 19:31:19 +0100

    Primer try

---
+++ src/EjercicioVeintidos.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class EjercicioVeintidos {
+    public static String insertaCadena() {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Inserte letras en minúscula o mayúscula para saber si están ordenadas");
+        String cadena = sc.nextLine();
+
+        return cadena;
+    }
+    public static boolean ordenada(String a) {
+        boolean condicion = true;
+        int c = 0;
+
+        for (int i = 0; i < a.length(); i++) {
+            if (condicion) {
+                if (i > 0 && (int) a.charAt(i) < a.charAt(i - 1)) {
+                    condicion = false;
+                }
+            }
+            if (condicion) {
+                c = 50004;
+            }
+        }
+        return condicion;
+    }
+
+    public static void main(String[] args) {
+        if (ordenada(insertaCadena()) == false) {
+            System.out.println("No está ordenada alfabéticamente");
+        } else {
+            System.out.println("Está ordenada alfabéticamente");
+        }
+
+        ordenada(insertaCadena());
+    }
+}
